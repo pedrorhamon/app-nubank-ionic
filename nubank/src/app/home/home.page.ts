ef25c2fdc38408fb3d51e8cf1660a115c79df7fe
Pedro Rhamon <pedrorhamon16@gmail.com>
2021-06-07 23:00:35 -0300

    incrementando o body, com icons

--- nubank/src/app/home/home.page.ts
+++ nubank/src/app/home/home.page.ts
@@ -19,6 +19,15 @@ export class HomePage {
 
   public slidesOptions: any = {slidesPerView: 3, freeMode: true};
 
+  public items: Array<any> = [
+    {icon: 'help-circle-outline', text: 'Me ajuda'},
+    {icon: 'person-outline', text: 'Perfil'},
+    {icon: 'cash-outline', text: 'Me ajuda'},
+    {icon: 'cash-outline', text: 'Configurar conta'},
+    {icon: 'card-outline', text: 'Configurar cartão'},
+    {icon: 'phone-portrait-outline', text: 'Configurações do app'},
+  ]
+
 
   constructor() {}
 
